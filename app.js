4d45f02ca69c55de661597ec9f09b03dd766087d
matthew-clay <alexandermike1999@gmail.com>
2022-01-26 20:40:13 +0630

    save in array[Obj] and added localStorage

--- app.js
+++ app.js
@@ -1,74 +1,98 @@
 /* Booking App JS */
-const inputTags = document.querySelectorAll(".customerInput");
+const inputTags = document.querySelectorAll(".customerInput"),
+	customerNameTag = document.querySelector("#customerName"),
+	serviceNameTag = document.querySelector("#serviceName"),
+	bookDateTag = document.querySelector("#bookDate"),
+	bookDateTimeTag = document.querySelector("#bookDateTime"),
+	btnSubmitTag = document.querySelector("#btnSubmit"),
+	bookInfoContainerTag = document.querySelector('.bookInfoContainer');
 
-const customerNameTag = document.querySelector("#customerName");
-const serviceNameTag = document.querySelector("#serviceName");
-const bookDateTag = document.querySelector("#bookDate");
-const bookTimeTag = document.querySelector("#bookTime");
-const btnSubmitTag = document.querySelector("#btnSubmit");
-
-
-const bookInfoContainerTag = document.querySelector('.bookInfoContainer');
-
-const clearPreviousInfo = (/*callback*/) => {
+const clearPreviousInfo = () => {
 	for (let i = 0; i < inputTags.length; i++) {
 		inputTags[i].value = "";
 	}
-	// callback();
 }
 
+const customerBookInfo = [];
+let bookInfoObj = {};
+let counter = 1;
 const saveCustomerBook = () => {
-	if (customerNameTag.value === "" && serviceNameTag === "" && bookDateTag === "") {
-		alert('Input Field Required...');
-		return alert;
+	const customerName = customerNameTag.value,
+		serviceName = serviceNameTag.value,
+		bookDate = bookDateTag.value,
+		bookDateTime = bookDateTimeTag.value;
+	
+	if (customerName === "" || serviceName === "" || bookDate === "" || bookDateTime === "") {
+		alert('Input Fields Required...');
+		return;
 	}
-	createShowBookedInfo();
+	
+	bookInfoObj = {
+		name: customerName,
+		serviceType: serviceName,
+		date: bookDate,
+		dateTime: bookDateTime,
+		id: counter,
+	};
+	customerBookInfo.push(bookInfoObj);
+	
+	
+	const key = `Booking ${counter}`;
+	const value = JSON.stringify(bookInfoObj);
+	localStorage.setItem(key, value);
+	counter += 1;
+	
+	bookInfoContainerTag.innerHTML = "";
+	createShowBookedInfo(customerBookInfo);
 }
 
+const createShowBookedInfo = (createShowBookedInfo) => {
+	
+	for (let i = 0; i < createShowBookedInfo.length; i++) {
+		const cardTag = document.createElement('div')
+		cardTag.classList.add('card', 'p-3', 'm-2');
+		
+		const bookTag = document.createElement('div')
+		bookTag.classList.add('text-center', 'bookID');
+		const idForBooking = customerBookInfo[i].id.toString();
+		bookTag.id = idForBooking;
+		bookTag.innerHTML = `Book ID - ${idForBooking}`;
+		
+		const showNameTag = document.createElement('div')
+		showNameTag.append(`Name - ${customerBookInfo[i].name}`)
+		
+		const showServiceTypeTag = document.createElement('div')
+		showServiceTypeTag.append(`Service Type - ${customerBookInfo[i].serviceType}`)
+		
+		const showDateInfoTag = document.createElement('div')
+		showDateInfoTag.append(`Book Date - ${customerBookInfo[i].date}`)
+		
+		const showTimeInfoTag = document.createElement('div')
+		showTimeInfoTag.append(`Time - ${customerBookInfo[i].dateTime}`);
+		
+		const editAndDeleteBookContainerTag = document.createElement('div')
+		editAndDeleteBookContainerTag.classList.add('btnContainer', 'mt-4')
+		const editBtnTag = document.createElement('button')
+		editBtnTag.classList.add('btn', 'btn-info')
+		editBtnTag.append('Edit Book');
+		
+		const deleteBtnTag = document.createElement('button')
+		deleteBtnTag.classList.add('btn', 'btn-danger', 'ms-2')
+		deleteBtnTag.append('Delete Book');
+		
+		editAndDeleteBookContainerTag.append(editBtnTag, deleteBtnTag);
+		cardTag.append(bookTag, showNameTag, showServiceTypeTag, showDateInfoTag, showTimeInfoTag, editAndDeleteBookContainerTag);
+		bookInfoContainerTag.append(cardTag);
+		console.log(customerBookInfo)
+	}
+	clearPreviousInfo();
+}
+
+
+btnSubmitTag.addEventListener('click', saveCustomerBook);
 
 window.addEventListener("load", () => {
 	clearPreviousInfo();
 });
 
-btnSubmitTag.addEventListener('click', saveCustomerBook);
-
-let counter = 0;
-const createShowBookedInfo = () => {
-	const cardTag = document.createElement('div')
-	cardTag.classList.add('card', 'p-3', 'm-2');
-	
-	const bookTag = document.createElement('div')
-	bookTag.classList.add('text-center', 'bookID');
-	const newCounter = counter + 1;
-	bookTag.id = newCounter.toString();
-	bookTag.innerHTML = `Book ID - ${newCounter}`;
-	
-	const showNameTag = document.createElement('div')
-	showNameTag.append(`Name - ${customerNameTag.value}`);
-	
-	const showServiceTypeTag = document.createElement('div')
-	showServiceTypeTag.append(`Service Type - ${serviceNameTag.value}`);
-	
-	const showDateInfoTag = document.createElement('div')
-	showDateInfoTag.append(`Book Date - ${bookDateTag.value}`)
-	
-	const showTimeInfoTag = document.createElement('div')
-	showTimeInfoTag.append(`Time - ${bookTimeTag.value}`);
-	
-	const editAndDeleteBookContainerTag = document.createElement('div')
-	editAndDeleteBookContainerTag.classList.add('btnContainer', 'mt-4')
-	const editBtnTag = document.createElement('button')
-	editBtnTag.classList.add('btn', 'btn-info')
-	editBtnTag.append('Edit Book');
-	
-	const deleteBtnTag = document.createElement('button')
-	deleteBtnTag.classList.add('btn', 'btn-danger', 'ms-2')
-	deleteBtnTag.append('Delete Book');
-	
-	editAndDeleteBookContainerTag.append(editBtnTag, deleteBtnTag);
-	cardTag.append(bookTag, showNameTag, showServiceTypeTag, showDateInfoTag, showTimeInfoTag, editAndDeleteBookContainerTag);
-	bookInfoContainerTag.append(cardTag);
-	
-	clearPreviousInfo();
-}
 
