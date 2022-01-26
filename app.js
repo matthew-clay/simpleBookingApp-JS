e180254b7e4eb5c55bb89d0bd26cc64461de6a34
matthew-clay <alexandermike1999@gmail.com>
2022-01-26 14:37:06 +0630

    finished prototyping for UI

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 /* Booking App JS */
+const inputTags = document.querySelectorAll(".customerInput");
+
+const customerNameTag = document.querySelector("#customerName");
+const serviceNameTag = document.querySelector("#serviceName");
+const bookDateTag = document.querySelector("#bookDate");
+const bookTimeTag = document.querySelector("#bookTime");
+const btnSubmitTag = document.querySelector("#btnSubmit");
+
+const bookInfoContainerTag = document.querySelector('.bookInfoContainer');
+
+const saveCustomerBook = () => {
+	if (customerNameTag.value === "") {
+		alert('Name Required...');
+		return alert
+	}
+}
+
+window.addEventListener("load", () => {
+});
+
+btnSubmitTag.addEventListener('click', saveCustomerBook);
+
+const createShowBookedInfo = () => {
+}
+
